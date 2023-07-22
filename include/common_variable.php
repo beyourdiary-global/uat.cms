4e9fbff3801e3d21ca3398a6eca0192db451503e
HayashiSora <138273204+lofhyssr09@users.noreply.github.com>
2023-07-22 09:40:36 +0800

    feat- add common_variable file

---
+++ include/common_variable.php
@@ -0,0 +1,11 @@
+<?php
+// define path
+define('img', './image/');
+
+// define image
+define('byd_logo', 'logo2.png');
+define('facebook', 'fb.png');
+define('twitter', 'twitter.png');
+define('linkedin', 'linkedin.png');
+define('instagram', 'instagram.png');
+?>
