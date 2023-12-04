d03b84829c79f3a0515cf76a8e9f7b9dc78f4a55
Sally foong <sally.foong@sgcarmart.com>
2023-12-04 09:06:38 +0800

    feat-remove the extra array and add the finance db connect to init file

--- init.php
+++ init.php
@@ -9,6 +9,7 @@ define('dbuser', 'root');
 define('dbpwd', '');
 define('dbhost', '127.0.0.1');
 define('dbname', 'byd_cms');
+define('dbFinance', 'beyourdi_financial');
 /* define('SITEURL', 'https://localhost'); */
 define('SITEURL', '//localhost/uat/uat.cms');
 $SITEURL = SITEURL;
@@ -106,6 +107,7 @@ define('EMPPERSONALINFO','employee_personal_info');
 define('TAG','tag');
 
 $connect = @mysqli_connect(dbhost, dbuser, dbpwd, dbname);
+$finance_connect = @mysqli_connect(dbhost, dbuser, dbpwd, dbFinance);
 
 //define session
 ?>
