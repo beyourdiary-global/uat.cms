25fad7f94b8dece75e68b68b1dacb660c1b573a7
Sally foong <sally.foong@sgcarmart.com>
2023-12-15 10:55:14 +0800

    feat- combine all the error log to one display file

--- error_log.php
+++ error_log.php
@@ -1,10 +1,15 @@
 <?php
 // Open the file
 $filePath = 'error_log'; // Replace with the actual path to your file
+$filePathFinance = 'finance/error_log'; // Replace with the actual path to your file
 
 // Read the content of the file
 $fileContent = file_get_contents($filePath);
+$fileContentFinance = file_get_contents($filePathFinance);
 
 // Display the content
 echo nl2br($fileContent);
+echo nl2br($fileContentFinance);
+
+
 ?>
