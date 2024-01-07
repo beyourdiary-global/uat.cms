f8f5335fb193e330d41294ef574af2bcea2b45e4
Natasia Ting <natasia.ting@beyourdiary.com>
2024-01-07 18:02:49 +0800

    Update initial_capital_trans.php

--- finance/initial_capital_trans.php
+++ finance/initial_capital_trans.php
@@ -331,7 +331,7 @@ if (($row_id) && !($act) && (USER_ID != '') && ($_SESSION['viewChk'] != 1) && ($
 
     </div>
 
-    <div id="CBAFormContainer" class="container d-flex justify-content-center">
+    <div id="INITCAFormContainer" class="container d-flex justify-content-center">
         <div class="col-6 col-md-6 formWidthAdjust">
             <form id="CBAForm" method="post" action="" enctype="multipart/form-data">
                 <div class="form-group mb-5">
