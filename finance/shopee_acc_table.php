23c68cb14815fd399cddeb44e0b8ff2c2496a873
JOANNBYD <155409353+JOANNBYD@users.noreply.github.com>
2024-01-18 02:49:31 +0800

    feat - create shopee_account and shopee_account table page

---
+++ finance/shopee_acc_table.php
@@ -0,0 +1,127 @@
+<?php
+$pageTitle = "Shopee Account";
+$isFinance = 1;
+include '../menuHeader.php';
+include '../checkCurrentPagePin.php';
+
+$pinAccess = checkCurrentPin($connect, $pageTitle);
+$_SESSION['act'] = '';
+$_SESSION['viewChk'] = '';
+$_SESSION['delChk'] = '';
+$num = 1;   // numbering
+
+$redirect_page = $SITEURL . '/finance/shopee_acc.php';
+$result = getData('*', '', '', SHOPEE_ACC, $finance_connect);
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+
+<script>
+    $(document).ready(() => {
+        createSortingTable('shopee_acc_table');
+    });
+</script>
+
+<body>
+
+    <div id="dispTable" class="container-fluid d-flex justify-content-center mt-3">
+
+        <div class="col-12 col-md-8">
+
+            <div class="d-flex flex-column mb-3">
+                <div class="row">
+                    <p><a href="<?= $SITEURL ?>/dashboard.php">Dashboard</a> <i class="fa-solid fa-chevron-right fa-xs"></i> <?php echo $pageTitle ?></p>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-between flex-wrap">
+                        <h2><?php echo $pageTitle ?></h2>
+                        <div class="mt-auto mb-auto">
+                            <?php if (isActionAllowed("Add", $pinAccess)) : ?>
+                                <a class="btn btn-sm btn-rounded btn-primary" name="addBtn" id="addBtn" href="<?= $redirect_page . "?act=" . $act_1 ?>"><i class="fa-solid fa-plus"></i> Add Account </a>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <table class="table table-striped" id="shopee_acc_table">
+                <thead>
+                    <tr>
+                        <th class="hideColumn" scope="col">ID</th>
+                        <th scope="col">Account Name</th>
+                        <th scope="col">Country</th>
+                        <th scope="col">Currency</th>
+                        <th scope="col" id="action_col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = $result->fetch_assoc()) {
+                    ?>
+
+                        <tr>
+                            <th class="hideColumn" scope="row"><?= $row['id'] ?></th>
+                            <td scope="row"><?= $row['accName'] ?></td>
+                            <td scope="row"><?= $row['country'] ?></td>
+                            <td scope="row"><?= $row['currency'] ?></td>
+                            <td scope="row">
+                                <div class="dropdown" style="text-align:center">
+                                    <a class="text-reset me-3 dropdown-toggle hidden-arrow" href="#" id="actionDropdownMenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <button id="action_menu_btn"><i class="fas fa-ellipsis-vertical fa-lg" id="action_menu"></i></button>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-left" aria-labelledby="actionDropdownMenu">
+                                        <li>
+                                            <?php if (isActionAllowed("View", $pinAccess)) : ?>
+                                                <a class="dropdown-item" href="<?= $redirect_page . "?id=" . $row['id'] ?>">View</a>
+                                            <?php endif; ?>
+                                        </li>
+                                        <li>
+                                            <?php if (isActionAllowed("Edit", $pinAccess)) : ?>
+                                                <a class="dropdown-item" href="<?= $redirect_page . "?id=" . $row['id'] . '&act=' . $act_2 ?>">Edit</a>
+                                            <?php endif; ?>
+                                        </li>
+                                        <li>
+                                            <?php if (isActionAllowed("Delete", $pinAccess)) : ?>
+                                                <a class="dropdown-item" onclick="confirmationDialog('<?= $row['id'] ?>',['<?= $row['accName'] ?>'],'<?= $pageTitle ?>','<?= $redirect_page ?>','<?= $SITEURL ?>/shopee_acc_table.php','D')">Delete</a>
+                                            <?php endif; ?>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th class="hideColumn" scope="col">ID</th>
+                        <th scope="col">Account Name</th>
+                        <th scope="col">Country</th>
+                        <th scope="col">Currency</th>
+                        <th scope="col" id="action_col">Action</th>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+
+    </div>
+
+</body>
+
+<script>
+    //Initial Page And Action Value
+    var page = "<?= $pageTitle ?>";
+    var action = "<?php echo isset($act) ? $act : ' '; ?>";
+
+    checkCurrentPage(page, action);
+   
+    dropdownMenuDispFix();
+
+    datatableAlignment('shopee_acc_table');
+</script>
+
+</html>
