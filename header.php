829319004236caf4c310ae65cd4da768beee8788
FAN KAI XUAN <kai.xuan@beyourdiary.com>
2024-01-19 12:07:50 +0800

    feat - update meta data at header

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!-- Meta Tags -->
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<meta name="description" content="Login to access dashboard">
+<meta name="description" content="<?= $row['meta'] ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=3.0">
 <meta name="robots" content="beyourdiary">
 
