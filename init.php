b6c056d0dd96ed9b4f621355156556e5207be6a0
JOANNBYD <155409353+JOANNBYD@users.noreply.github.com>
2024-02-19 11:33:05 +0800

    feat - create chanel social media page

--- init.php
+++ init.php
@@ -143,6 +143,7 @@ define('FB_ORDER_REQ', 'facebook_order_request');
 define('FB_PAGE_ACC', 'facebook_page_account');
 // define('CHANNEL', 'channel');
 define('STK_CDT_TOPUP_RCD', 'stock_credit_topup_record');
+define('CHANEL_SC_MD', 'chanel_social_media');
 
 $connect = @mysqli_connect(dbhost, dbuser, dbpwd, dbname);
 $finance_connect = @mysqli_connect(dbhost, dbuser, dbpwd, dbFinance);
