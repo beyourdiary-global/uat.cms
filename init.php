64a34827e384aa4df0ec2bf9a5bab016b2d0f541
JOANNBYD <155409353+JOANNBYD@users.noreply.github.com>
2024-03-01 14:06:10 +0800

    feat - create stripe transaction page

--- init.php
+++ init.php
@@ -150,7 +150,7 @@ define('CHANEL_SC_MD', 'chanel_social_media');
 define('SHOPEE_SCR_SETT','shopee_service_charges_rate_setting');
 define('SHOPEE_SG_SETT','shopee_sg_fees_setting');
 define('SHOPEE_CUST_INFO','shopee_customer_info');
-
+define('STRIPE_TRANS_BACKUP', 'stripe_transaction_backup');
 
 $connect = @mysqli_connect(dbhost, dbuser, dbpwd, dbname);
 $finance_connect = @mysqli_connect(dbhost, dbuser, dbpwd, dbFinance);
