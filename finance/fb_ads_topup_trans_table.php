71c686b82097b2da7e2bbbd49393132292038d3c
LeonardLuLL <leonard.lu@beyourdiary.com>
2024-03-05 23:03:56 +0800

    feat(Make filter to filter the table)

--- finance/fb_ads_topup_trans_table.php
+++ finance/fb_ads_topup_trans_table.php
@@ -20,13 +20,14 @@ $result = getData('*', '', '', FB_ADS_TOPUP, $finance_connect);
 <head>
     <link rel="stylesheet" href="../css/main.css">
 </head>
-
 <script>
     $(document).ready(() => {
         createSortingTable('fb_ads_topup_trans_table');
     });
 </script>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <body>
 
     <div id="dispTable" class="container-fluid d-flex justify-content-center mt-3">
@@ -49,7 +50,56 @@ $result = getData('*', '', '', FB_ADS_TOPUP, $finance_connect);
                     </div>
                 </div>
             </div>
+            <div class="row mb-3">
+                    <div class="col">
+                        <label for="timeInterval" class="form-label">Filter by:</label>
+                        <select class="form-select" id="timeInterval">
+                            <option value="daily">Daily</option>
+                            <option value="weekly">Weekly</option>
+                            <option value="monthly">Monthly</option>
+                            <option value="yearly">Yearly</option>
+                        </select>
+                    </div>
+                    
+                <div class="col">
 
+                        <label for="dateFilter" class="form-label">Filter by Payment Date:</label>
+                        <div class="input-group date" id="datepicker">
+                            <input type="text" class="form-control" placeholder="Select date">
+                            <div class="input-group-addon">
+                                <span class="glyphicon glyphicon-th"></span>
+                            </div>
+                        </div>
+
+                        <div class="input-daterange input-group" id="datepicker2" style="display: none;">
+                            <input type="text" class="input form-control" name="start" placeholder="Start date"/>
+                            <span class="input-group-addon date-separator"> to </span>
+                            <input type="text" class="input-sm form-control" name="end" placeholder="End date"/>
+                        </div>
+                        <div class="input-group input-daterange" id="datepicker3" style="display: none;">
+                            <input type="text" class="input form-control" name="start" placeholder="Start month"/>
+                            <span class="input-group-addon date-separator"> to </span>
+                            <input type="text" class="input-sm form-control" name="end" placeholder="End month"/>
+                           
+                        </div>
+                        <div class="input-group input-daterange" id="datepicker4" style="display: none;">
+                             <input type="text" class="input form-control" name="start" placeholder="Start year"/>
+                            <span class="input-group-addon date-separator"> to </span>
+                            <input type="text" class="input-sm form-control" name="end" placeholder="End year"/>
+                           
+                        </div>
+                    </div>
+                    
+            </div>
+            <div class="row mb-3">
+            <div class="col-md-6">
+                        <label>Group by:</label>
+                        <select class="form-select" id="group">
+                            <option value="">-</option>
+                          
+                        </select>
+                    </div>
+            </div>
             <table class="table table-striped" id="fb_ads_topup_trans_table">
                 <thead>
                     <tr>
@@ -66,7 +116,7 @@ $result = getData('*', '', '', FB_ADS_TOPUP, $finance_connect);
                     </tr>
                 </thead>
                 <tbody>
-                    <?php while ($row = $result->fetch_assoc()) {
+                <?php while ($row = $result->fetch_assoc()) {
                         if (isset($row['transactionID'], $row['id']) && !empty($row['transactionID'])) {
                             $metaQuery = getData('*', "id='" . $row['meta_acc'] . "'", '', META_ADS_ACC, $finance_connect);
                             $meta_acc = $metaQuery->fetch_assoc();
@@ -81,8 +131,8 @@ $result = getData('*', '', '', FB_ADS_TOPUP, $finance_connect);
                                 <td scope="row"><?php if (isset($row['payment_date'])) echo $row['payment_date'] ?></td>
                                 <td scope="row"><?php if (isset($usr['name'])) echo $usr['name'] ?></td>
                                 <td scope="row"><?php if (isset($row['topup_amt'])) echo  $row['topup_amt'] ?></td>
-                                <td scope="row"><?php if (isset($row['remark'])) echo $row['remark'] ?></td>
                                 <td scope="row"><?php if (isset($row['attachment'])) echo $row['attachment'] ?></td>
+                                <td scope="row"><?php if (isset($row['remark'])) echo $row['remark'] ?></td>     
                                 <td scope="row">
                                     <div class="dropdown" style="text-align:center">
                                         <a class="text-reset me-3 dropdown-toggle hidden-arrow" href="#" id="actionDropdownMenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -131,7 +181,145 @@ $result = getData('*', '', '', FB_ADS_TOPUP, $finance_connect);
     </div>
 
 </body>
+<script>$(document).ready(function() {
+    // Initialize the datepickers
+    $('#datepicker').datepicker({
+        autoclose: true,
+        format: 'yyyy-mm-dd',
+        weekStart: 1,
+        maxViewMode: 0, // Set to show only days
+        minViewMode: 0, // Set to show only days
+        todayHighlight: true,
+        toggleActive: true,
+        orientation: 'bottom left',
+    });
+
+
+    $('#datepicker2').datepicker({
+        autoclose: true,
+        format: 'yyyy-mm-dd',
+        weekStart: 1,
+        maxViewMode: 1,
+        todayHighlight: true,
+        toggleActive: true,
+        orientation: 'bottom',
+    });
+
+        
+    $('#datepicker3').datepicker({
+        format: "yyyy-mm",
+        minViewMode: 1,
+        autoclose: true,
+        orientation: 'bottom',
+    });
+
+    $('#datepicker4').datepicker({
+        format: "yyyy",
+        minViewMode: 2,
+        autoclose: true,
+        orientation: 'bottom',
+    });
+
+    // Function to filter the table based on the selected date range
+    function filterTable() {
+        var selectedOption = $('#timeInterval').val();
+        if (selectedOption === 'daily') {
+            var selectedDate = $('#datepicker input').val();
+            $('#fb_ads_topup_trans_table tbody tr').each(function() {
+                var paymentDate = $(this).find('td:nth-child(5)').text();
+                if (paymentDate === selectedDate) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        } 
+        else if (selectedOption === 'weekly') {
+            var startDate = $('#datepicker2 input[name="start"]').val();
+            var endDate = new Date(startDate);
+            endDate.setDate(endDate.getDate() + 6); // Add 6 days to get a total of 7 days
+            var endDateFormatted = endDate.toISOString().split('T')[0]; // Format the date as yyyy-mm-dd
+            $('#datepicker2 input[name="end"]').val(endDateFormatted);
+
+            $('#fb_ads_topup_trans_table tbody tr').each(function() {
+                var paymentDate = $(this).find('td:nth-child(5)').text();
+                if ((startDate === '' || paymentDate >= startDate) && (endDateFormatted === '' || paymentDate <= endDateFormatted)) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        }
+
+        else if (selectedOption === 'monthly') {
+            var startDate = $('#datepicker2').datepicker('getDate');
+            var endDate = new Date(startDate);
+            endDate.setDate(startDate.getDate() + 7);
+            $('#fb_ads_topup_trans_table tbody tr').each(function() {
+                var paymentDate = $(this).find('td:nth-child(5)').text();
+                if ((startDate === '' || paymentDate >= startDate) && (endDate === '' || paymentDate <= endDate)) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        }
+        else if (selectedOption === 'yearly') {
+        var startYear = $('#datepicker4 input[name="start"]').val();
+        var endYear = $('#datepicker4 input[name="end"]').val();
+        $('#fb_ads_topup_trans_table tbody tr').each(function() {
+            var paymentYear = $(this).find('td:nth-child(5)').text().slice(0, 4);
+            if ((startYear === '' || paymentYear >= startYear) && (endYear === '' || paymentYear <= endYear)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+            });
+        }
+
+    }
+
+    // Filter the table when the date range changes
+    $('#datepicker, #datepicker2, #datepicker3, #datepicker4').on('changeDate', filterTable);
+
+    // Filter the table when the time interval changes
+    $('#timeInterval').change(function() {
+        var selectedOption = $(this).val();
+        if (selectedOption === 'daily') {
+            $('#datepicker').prop('disabled', false).show();
+            $('#datepicker2').prop('disabled', true).hide();
+            $('#datepicker3').prop('disabled', true).hide();
+            $('#datepicker4').prop('disabled', true).hide();
+        } else if (selectedOption === 'weekly') {
+            $('#datepicker').prop('disabled', true).hide();
+            $('#datepicker2').prop('disabled', false).show();
+            $('#datepicker3').prop('disabled', true).hide();
+            $('#datepicker4').prop('disabled', true).hide();
+        } else if (selectedOption === 'monthly') {
+            $('#datepicker').prop('disabled', true).hide();
+            $('#datepicker2').prop('disabled', true).hide();
+            $('#datepicker3').prop('disabled', false).show();
+            $('#datepicker4').prop('disabled', true).hide();
+        } else if (selectedOption === 'yearly') {
+            $('#datepicker').prop('disabled', true).hide();
+            $('#datepicker2').prop('disabled', true).hide();
+            $('#datepicker3').prop('disabled', true).hide();
+            $('#datepicker4').prop('disabled', false).show();
+        }
+        filterTable();
+    });
+
+    // Initial hide based on selected option
+    
+});
+</script>
+
+
+
+
 <script>
+
+    
     /**
   oufei 20231014
   common.fun.js
