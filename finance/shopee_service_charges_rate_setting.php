4908e445410baa9fb59560dec1ea243ed75b7133
JOANNBYD <155409353+JOANNBYD@users.noreply.github.com>
2024-03-22 11:59:40 +0800

    feat - resolve error

--- finance/shopee_service_charges_rate_setting.php
+++ finance/shopee_service_charges_rate_setting.php
@@ -401,6 +401,13 @@ if (($dataID) && !($act) && (USER_ID != '') && ($_SESSION['viewChk'] != 1) && ($
         setAutofocus(action);
         setButtonColor();
         preloader(300, action);
+
+        function isNumberKey(evt) {
+        var charCode = (evt.which) ? evt.which : event.keyCode;
+        if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+        return true;
+}
         <?php include "../js/shopee_service_charges_rate_setting.js" ?>
     </script>
 
